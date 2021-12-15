ef5e421730684c3d2cbd9491878ffa26c1b23ce5
billy <wbfore98@gmail.com>
2021-12-14 21:44:28 -0500

    DOM setup

--- book-constructor.js
+++ book-constructor.js
@@ -1,6 +1,8 @@
 let myLibrary = [];
 
-const addButton = document.querySelector('#addButton');
+const newBookButton = document.querySelector('#newBookButton');
+const addToLibButton = document.querySelector('#addToLibButton');
+const clearLibButton = document.querySelector('#clearLibButton');
 const booklist = document.querySelector('#booklist');
 
 function Book(title, author, pages, read) {
@@ -8,46 +10,58 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  
-  this.info = function() {
-      if (this.read == 1) {
-        return (this.title + " by " + this.author + ", " +  this.pages + " pages, has been read.");
-        }
-      else {
-        return (title + " by " + author + ", " + pages + " pages, not read yet.");
-        } 
-  }
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 } 
 
-function displayBooks() {
-  console.log("displaying books");
-  for(let i = 0; i < myLibrary.length; i++) {
-    const newCard = document.createElement('div');
-    newCard.append(myLibrary[i].title);
-    newCard.append(myLibrary[i].author);
-    newCard.append(myLibrary[i].pages);
-    newCard.append(myLibrary[i].read);
-    newCard.append(myLibrary[i].info());
-    booklist.append(newCard);
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
-  
 }
 
-let endersGame = new Book("Ender's Game", "Orson Scott Card", "999", 1);
-let goDogGo = new Book("Go, Dog. Go!", "P.D. Eastman", "7", 1);
+function clearLibrary() {
+  myLibrary = [];
+  removeAllChildNodes(booklist);
+}
 
-addBookToLibrary(endersGame);
-addBookToLibrary(goDogGo);
+function displayBook() {
+  removeAllChildNodes(booklist);  
+  for(let i = 0; i < myLibrary.length; i++) {
+    const newCard = document.createElement('div');
+    const newTitle = document.createElement('p');
+    const newAuthor = document.createElement('p');
+    const newPages = document.createElement('p');
+    const newRead = document.createElement('p');
+    //newCard.className = "card";
+    newTitle.textContent = myLibrary[i].title;
+    newAuthor.textContent = myLibrary[i].author;
+    newPages.textContent = myLibrary[i].pages;
+    newRead.textContent = myLibrary[i].read;
+    
+    newCard.appendChild(newTitle);
+    newCard.appendChild(newAuthor);
+    newCard.appendChild(newPages);
+    newCard.appendChild(newRead);
+    booklist.appendChild(newCard);
+  }
+}
 
-let flag = 0;
+addToLibButton.addEventListener('click', function(event) {
+  //var newBook = formToBook();
+  //addBookToLibrary(newBook); 
+  displayBook();
+  //closeForm();
+});
+ 
+newBookButton.addEventListener('click', function(event) {
+  //openForm(); 
+  let test = new Book("Ender's Game","Orson Scott Card","999",1);
+  addBookToLibrary(test);
+}); 
 
-addButton.addEventListener('click', function(event) {
-  const newCard = document.createElement('div');
-  console.log("weiner mode");
-  displayBooks();
-  booklist.append(newCard); 
+clearLibButton.addEventListener('click', function(event) {
+  clearLibrary();
 });
